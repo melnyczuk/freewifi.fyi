5fe1dbbefe54b55bdf34386e199395ffbda89c21
melnyczuk <h.melnyczuk@gmail.com>
2017-11-09 08:32:43 +0800

    initial commit

---
+++ ratioclub/workshops/week_1/week_1.js
@@ -0,0 +1,17 @@
+function setup() { createCanvas(window.innerWidth, window.innerHeight);
+    background(0, 0, 0); }
+
+function draw() { ellipse(100, 100, 100, 100); } <
+script src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/p5.min.js" > < /script> <
+    /head>
+
+<
+body >
+    <
+    script src = "week_1.js" >
+    <
+    /script> <
+    /body>
+
+<
+/html>
