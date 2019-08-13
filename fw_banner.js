39617445de406892f8d77d9b8e8d1c66ba9cfbd5
how <h.melnyczuk@gmail.com>
2019-08-13 11:05:28 +0200

    updates

--- fw_banner.js
+++ fw_banner.js
@@ -1,36 +1,36 @@
-var draw = SVG('banner').size('100%', '100%');
+var draw = SVG('banner').size('90%', '90%');
 var text = draw
-    .text("FREE WIFI")
-    .fill('#0025EE')
-    .font({ size: 100, family: 'Roboto Mono' })
-    .path('M ' + 0 + ' ' + 0 + ' C ' + 500 + ' ' + 0 + ' ' + 0 + ' ' + 0 + ' ' + 1000 + ' ' + 0);
+  .text("FREE")// WIFI")
+  .fill('#0025EE')
+  .font({ size: 100, family: 'Roboto Mono' })
+  .path('M ' + 0 + ' ' + 0 + ' C ' + 500 + ' ' + 0 + ' ' + 0 + ' ' + 0 + ' ' + 1000 + ' ' + 0);
 
-var drop = function() {
-    text.animate(2000, '<>')
-        .plot(
-            'M ' +
-            0 +
-            ' ' +
-            Math.round(window.innerHeight * (2 / 3)) +
-            ' C ' +
-            0 +
-            ' ' +
-            0 +
-            ' ' +
-            0 +
-            ' ' +
-            0 +
-            ' ' +
-            0 +
-            ' ' +
-            0
-        );
+var drop = function () {
+  text.animate(2000, '<>')
+    .plot(
+    'M ' +
+    0 +
+    ' ' +
+    Math.round(window.innerHeight * (2 / 3)) +
+    ' C ' +
+    0 +
+    ' ' +
+    0 +
+    ' ' +
+    0 +
+    ' ' +
+    0 +
+    ' ' +
+    0 +
+    ' ' +
+    0
+    );
 };
 
-document.addEventListener('scroll', function(event) {
-    drop();
+document.addEventListener('scroll', function (event) {
+  drop();
 });
 
-document.addEventListener('click', function(event) {
-    drop();
+document.addEventListener('click', function (event) {
+  drop();
 });
