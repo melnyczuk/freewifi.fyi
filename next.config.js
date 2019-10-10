af957ef59340aebf581928b46d7a7158b3c033e6
how <h.melnyczuk@gmail.com>
2019-10-10 10:31:44 +0100

    Setup static export

--- next.config.js
+++ next.config.js
@@ -1,17 +1,24 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass({
-   webpack (config, options) {
-       config.module.rules.push({
-           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-           use: {
-               loader: 'url-loader',
-               options: {
-                   limit: 100000
-               }
-           }
-       });
 
-       return config;
-   }
+module.exports = withCSS(withSass({
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      projects: { page: '/projects' },
+      ratioclub: { page: '/ratioclub' },
+    };
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    });
+    return config;
+  }
 }));
