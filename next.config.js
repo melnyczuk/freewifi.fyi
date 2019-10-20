44fcc5f16d0d90f7d28a4ce4b701a364c931977b
how <h.melnyczuk@gmail.com>
2019-10-20 20:44:11 +0100

    Fix  static rendering

--- next.config.js
+++ next.config.js
@@ -3,12 +3,21 @@ const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS(withSass({
   exportPathMap() {
+    const weekPath = '/ratioclub/workshops/week_';
+    const workshops = new Array(7).fill(0).reduce(
+      (prev, _, i) => ({ [weekPath + i]: { page: weekPath + i }, ...prev, }),
+      {}
+    );
+
     return {
       '/': { page: '/' },
-      projects: { page: '/projects' },
-      ratioclub: { page: '/ratioclub' },
+      '/projects': { page: '/projects' },
+      '/ratioclub': { page: '/ratioclub' },
+      '/ratioclub/showcase': { page: '/ratioclub/showcase' },
+      ...workshops
     };
   },
+
   webpack(config, options) {
     config.module.rules.push({
       test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
